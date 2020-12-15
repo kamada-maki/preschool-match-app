90eee8e777b884755ff46de7f336edaa5cf74c80
kamada-maki <ikam.adamak17@gmail.com>
2020-12-15 20:03:35 +0900

    adminモデル、コントローラー追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,6 @@ class ApplicationController < ActionController::Base
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer
-    .permit(:sign_up, keys:[:nickname])
+    .permit(:sign_up, keys:[:nickname,:name])
   end
 end
