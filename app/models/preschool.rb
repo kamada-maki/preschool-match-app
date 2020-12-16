13a05bede7e766cad6a2584ea7382f89fa4c0fb9
kamada-maki <ikam.adamak17@gmail.com>
2020-12-16 11:36:05 +0900

    preschoolモデルにバリデーション記述

--- app/models/preschool.rb
+++ app/models/preschool.rb
@@ -1,2 +1,20 @@
 class Preschool < ApplicationRecord
+  validates :area_id,:category_id,numericality:{other_than:1}
+  belongs_to :area
+  belongs_to :category
+  belongs_to :admin
+  validates :name, length: { maximum: 20 }
+  validates :post_number ,format:{ with: /\A\d{3}[-]\d{4}\z/}
+  validates :phone_number,format:{ with: /\A[0-9]+\z/},length: { maximum: 11 }
+  
+  with_options presence: true do
+  validates :name
+  validates :post_number
+  validates :street_number
+  validates :building
+  validates :phone_number
+  validates :open_hour
+  validates :close_hour
+  validates :capacity 
+  validates :concept
 end
