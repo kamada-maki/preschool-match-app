068f254cf61ee3e25b2c78d52d49507eb6603087
kamada-maki <ikam.adamak17@gmail.com>
2020-12-16 12:20:04 +0900

    preschoolコントローラー作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_105612) do
+ActiveRecord::Schema.define(version: 2020_12_16_021256) do
 
   create_table "admins", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,25 @@ ActiveRecord::Schema.define(version: 2020_12_15_105612) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "preschools", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "post_number", null: false
+    t.integer "area_id", null: false
+    t.string "street_number", null: false
+    t.string "building"
+    t.string "phone_number", null: false
+    t.string "station", null: false
+    t.time "open_hour", null: false
+    t.time "close_hour", null: false
+    t.integer "capacity", null: false
+    t.integer "category_id", null: false
+    t.string "concept", null: false
+    t.bigint "admin_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["admin_id"], name: "index_preschools_on_admin_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -38,4 +57,5 @@ ActiveRecord::Schema.define(version: 2020_12_15_105612) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "preschools", "admins"
 end
