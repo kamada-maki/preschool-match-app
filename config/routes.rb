4e4b43e987b1afb3b198dc2054063d0986aebfd4
kamada-maki <ikam.adamak17@gmail.com>
2020-12-17 10:20:25 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
-  resources :preschools
   root to: 'preschools#index'
+
+  resources :preschools do
+    resources :comments, only: :create,:edit,:update,:destroy
+  end
 end
