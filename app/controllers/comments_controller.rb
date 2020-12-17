97f749b85f271f5f825a83ea7b4b08888a306dd7
kamada-maki <ikam.adamak17@gmail.com>
2020-12-17 12:31:34 +0900

    preschoolのshowにコメント機能実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,28 @@
 class CommentsController < ApplicationController
   def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      redirect_to preschool_path(@comment.preschool)
+    else
+      @preschool = @comment.preschool
+      @comments = @preschool.comments
+      render "preschools/show"
+    end
   end
   def edit
   end
   def update
   end
   def destroy
+    @preschool =Preschool.find(params[:preschool_id])
+    @comment =Comment.find(params[:id])
+    @comment.destroy
+    redirect_to preschool_path(@preschool.id)
+  end
+
+  private
+  def comment_params
+    params.require(:comment)
+    .permit(:text).merge(user_id: current_user.id,preschool_id: params[:preschool_id])
   end
 end
