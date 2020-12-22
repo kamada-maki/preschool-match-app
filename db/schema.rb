0264c2d95b1c15b9a0059437c8b2cf0a1093464c
kamada-maki <ikam.adamak17@gmail.com>
2020-12-22 12:25:01 +0900

    gemインストール

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_16_103908) do
+ActiveRecord::Schema.define(version: 2020_12_22_022700) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -75,6 +75,15 @@ ActiveRecord::Schema.define(version: 2020_12_16_103908) do
     t.index ["admin_id"], name: "index_preschools_on_admin_id"
   end
 
+  create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "provider"
+    t.string "uid"
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_sns_credentials_on_user_id"
+  end
+
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -92,4 +101,5 @@ ActiveRecord::Schema.define(version: 2020_12_16_103908) do
   add_foreign_key "comments", "preschools"
   add_foreign_key "comments", "users"
   add_foreign_key "preschools", "admins"
+  add_foreign_key "sns_credentials", "users"
 end
