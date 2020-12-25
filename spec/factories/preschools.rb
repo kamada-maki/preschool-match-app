993196da2e19de7e6801d80e99696990be9091ee
kamada-maki <ikam.adamak17@gmail.com>
2020-12-25 17:36:39 +0900

    preschoolモデルテストコード修正

--- spec/factories/preschools.rb
+++ spec/factories/preschools.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :preschool do
     name              {Faker::Name.last_name}
     post_number       {"123-4567"}
-    area_id           {2}
+    area_id           {1}
     street_number     {Faker::Address.street_address}
     building          {Faker::Address.secondary_address}
     phone_number      {"09012345678"}
@@ -10,7 +10,7 @@ FactoryBot.define do
     open_hour         {Faker::Number.decimal_part(digits: 2)}
     close_hour        {Faker::Number.decimal_part(digits: 2)}
     capacity          {Faker::Number.number(digits: 10)}
-    category_id       {2}
+    category_id       {1}
     concept          {Faker::Lorem.sentence(1..150)}
     association :admin
   end
