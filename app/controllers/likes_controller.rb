de768d653ee6e5959ae56207e36b0df11d334839
kamada-maki <ikam.adamak17@gmail.com>
2020-12-29 12:14:21 +0900

    preschool→@preschoolに変更

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,18 +3,23 @@ class LikesController < ApplicationController
   def create
     @preschool =Preschool.find(params[:preschool_id])
     @like = current_user.likes.create(preschool_id: params[:preschool_id])
-    redirect_back(fallback_location: root_path)
     @like.save
-    @user = User.find(@comment.user_id)
+    @user = User.find(@like.user_id)
     html = render_to_string layout: false
     render json:{ like: @like, user: @user, html: html }
   end
   def destroy
     @preschool =Preschool.find(params[:preschool_id])
     @like = current_user.likes.find_by(preschool_id: @preschool.id)
-    @like.destroy(params[:id])
-    @user = User.find(@comment.user_id)
+    @like.destroy
+    @user = User.find(@like.user_id)
     html = render_to_string layout: false
     render json:{ like: @like, user: @user, html: html }
   end
+
+  # private
+  # def like_params
+  #   params.require(:like)
+  #   .permit(:id).merge(user_id: current_user.id,preschool_id: params[:preschool_id])
+  # end
 end
