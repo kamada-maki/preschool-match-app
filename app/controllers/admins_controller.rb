086074f482944026fdbaae48be8682e50aa181e8
kamada-maki <ikam.adamak17@gmail.com>
2021-01-08 19:45:46 +0900

    運営者管理ページのバリデーション

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,4 +1,5 @@
 class AdminsController < ApplicationController
+  before_action :authenticate_admin!, only:[:show]
   def show
     @admin = Admin.find(params[:id])
     @preschools = @admin.preschools
