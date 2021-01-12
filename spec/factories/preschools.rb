d8635459b911c16e270f1402c2c4cf5a4eec9fb6
kamada-maki <ikam.adamak17@gmail.com>
2021-01-12 11:14:04 +0900

    modelテスト修正

--- spec/factories/preschools.rb
+++ spec/factories/preschools.rb
@@ -3,7 +3,7 @@ FactoryBot.define do
     name              { Faker::Name.last_name }
     post_number       { '123-4567' }
     area_id           { 1 }
-    street_number     { Faker::Address.street_address }
+    address { Faker::Address.street_address }
     building          { Faker::Address.secondary_address }
     phone_number      { '09012345678' }
     access            { Faker::Lorem.sentence(1..30) }
@@ -11,6 +11,7 @@ FactoryBot.define do
     close_hour        { Faker::Number.decimal_part(digits: 2) }
     capacity          { Faker::Number.number(digits: 10) }
     category_id       { 1 }
+    email                 { Faker::Internet.free_email }
     concept { Faker::Lorem.sentence(1..150) }
     association :admin
   end
