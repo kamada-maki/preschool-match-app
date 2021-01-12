daf6c4dc0998ea461392a69f17ad6c9aba3185d8
kamada-maki <ikam.adamak17@gmail.com>
2021-01-12 18:22:34 +0900

    シュミレーション機能メソッド作成（仮）

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,37 @@
 module ApplicationHelper
+    def max_price ##上限残り金額
+    if @price.category_id == 1 or 4
+      puts @price.free_price
+    elsif @price.category_id ==3 && @price.age =0 or 1 or 2
+      puts 42000
+    elsif @price.category_id ==3 && @price.age =3 or 4 or 5
+      puts 37000
+    elsif @price.category_id ==2 && @price.age =0
+      puts 37100
+    elsif @price.category_id ==2 && @price.age =1 or 2
+      puts 37100
+    elsif @price.category_id ==2 && @price.age = 3
+      puts 26600
+    elsif @price.category_id ==2 && @price.age = 4 or 5
+      puts 23100
+    end
+  end
+
+  def target_price #対象金額
+    if @price.category_id == 1 or 4
+      puts @price.free_price
+    elsif @price.category_id ==3 && @price.age =0 or 1 or 2
+      puts 42000 - @price.free_price
+    elsif @price.category_id ==3 && @price.age =3 or 4 or 5
+      puts 37000 - @price.free_price
+    elsif @price.category_id ==2 && @price.age =0
+      puts 37100 - @price.free_price
+    elsif @price.category_id ==2 && @price.age =1 or 2
+      puts 37100 - @price.free_price
+    elsif @price.category_id ==2 && @price.age = 3
+      puts 26600 - @price.free_price
+    elsif @price.category_id ==2 && @price.age = 4 or 5
+      puts 23100 - @price.free_price
+    end
+  end
 end
