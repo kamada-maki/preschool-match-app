d00406ebc3fc6995eed6169ef6ef34c3700a7207
kamada-maki <ikam.adamak17@gmail.com>
2021-01-13 11:02:08 +0900

    シュミレーションのメソッド作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,37 +1,45 @@
 module ApplicationHelper
-    def max_price ##上限残り金額
-    if @price.category_id == 1 or 4
-      puts @price.free_price
-    elsif @price.category_id ==3 && @price.age =0 or 1 or 2
-      puts 42000
-    elsif @price.category_id ==3 && @price.age =3 or 4 or 5
-      puts 37000
-    elsif @price.category_id ==2 && @price.age =0
-      puts 37100
-    elsif @price.category_id ==2 && @price.age =1 or 2
-      puts 37100
-    elsif @price.category_id ==2 && @price.age = 3
-      puts 26600
-    elsif @price.category_id ==2 && @price.age = 4 or 5
-      puts 23100
+  def target_price #対象金額
+    if @price.category_id == 1 or @price.category_id == 4 
+      return @price.free_price
+    elsif @price.category_id == 3 && @price.age < 3 && @price.free_price <= 42000
+      return @price.free_price
+    elsif @price.category_id == 3 && @price.age < 3 && @price.free_price > 42000
+      return 42000
+    elsif @price.category_id == 3 && @price.age >= 3 && @price.free_price <= 37000
+      return @price.free_price
+    elsif @price.category_id == 3 && @price.age >= 3 && @price.free_price > 37000
+      return 37000
+    elsif @price.category_id == 2 && @price.age ==  0 && @price.free_price <= 37100
+      return @price.free_price
+    elsif @price.category_id == 2 && @price.age ==  0 && @price.free_price > 37100
+      return 37100
+    elsif @price.category_id == 2 && @price.age ==  1 || 2 && @price.free_price <= 37000
+      return @price.free_price
+    elsif @price.category_id == 2 && @price.age ==  1 || 2 && @price.free_price > 37000
+      return 37000
+    elsif @price.category_id == 2 && @price.age ==  3 && @price.free_price <= 26600
+      return @price.free_price
+    elsif @price.category_id == 2 && @price.age ==  3 && @price.free_price > 26600
+      return 26600
+    elsif @price.category_id == 2 && @price.age > 3 && @price.free_price <= 23100
+      return @price.free_price
+    elsif @price.category_id == 2 && @price.age > 3 && @price.free_price > 23100
+      return 23100
     end
   end
 
-  def target_price #対象金額
-    if @price.category_id == 1 or 4
-      puts @price.free_price
-    elsif @price.category_id ==3 && @price.age =0 or 1 or 2
-      puts 42000 - @price.free_price
-    elsif @price.category_id ==3 && @price.age =3 or 4 or 5
-      puts 37000 - @price.free_price
-    elsif @price.category_id ==2 && @price.age =0
-      puts 37100 - @price.free_price
-    elsif @price.category_id ==2 && @price.age =1 or 2
-      puts 37100 - @price.free_price
-    elsif @price.category_id ==2 && @price.age = 3
-      puts 26600 - @price.free_price
-    elsif @price.category_id ==2 && @price.age = 4 or 5
-      puts 23100 - @price.free_price
+  def max_price ##上限残り金額
+    if @price.category_id != 3
+      return  "他の施設を無償化対象としての併用はできません。"
+    elsif @price.category_id == 3 && @price.age < 3 && target_price <= 42000
+      return "#{42000 - target_price}円"
+    elsif @price.category_id == 3 && @price.age < 3 && target_price > 42000
+      return "0円"
+    elsif @price.category_id == 3 && @price.age >= 3 && target_price <= 37000
+      return "#{37000 - target_price}円"
+    elsif @price.category_id == 3 && @price.age >= 3 && target_price > 37000
+      return  "0円"
     end
   end
 end
