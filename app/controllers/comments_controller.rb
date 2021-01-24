76870b700e2cb78dd0393de52f736c68802c46be
kamada-maki <ikam.adamak17@gmail.com>
2021-01-24 13:51:52 +0900

    commentコントローラーリファクタリング

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
+  before_action :set_preschool 
   def create
-    @preschool = Preschool.find(params[:preschool_id])
     @comment = Comment.new(comment_params)
     if @comment.save
       @user = User.find(@comment.user_id)
@@ -11,25 +11,20 @@ class CommentsController < ApplicationController
     end
     render json: { comment: @comment, user: @user, html: html }
   end
-
-  def edit
-  end
-
-  def update
-  end
-
   def destroy
-    @preschool = Preschool.find(params[:preschool_id])
     @comment = Comment.destroy(params[:id])
     @user = User.find(@comment.user_id)
     html = render_to_string layout: false
     render json: { comment: @comment, user: @user, html: html }
   end
 
-  private
 
+  private
   def comment_params
     params.require(:comment)
           .permit(:text).merge(user_id: current_user.id, preschool_id: params[:preschool_id])
+  end
+  def set_preschool
+    @preschool = Preschool.find(params[:preschool_id])
   end
 end
