2818b2f43b63964f95c294f02cc233a0cc2195f7
kamada-maki <ikam.adamak17@gmail.com>
2021-01-24 11:17:51 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,9 @@ Rails.application.routes.draw do
   resources :beginners, only:[:index]
   resources :categories, only:[:index]
   root to: 'preschools#index'
-  
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
   resources :users, only: [:show] do
     get :likes, on: :collection
   end
