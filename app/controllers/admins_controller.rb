824306db15a755b6de4a0df1c9fde387785bc826
kamada-maki <ikam.adamak17@gmail.com>
2021-01-24 13:45:37 +0900

    likekコントローラーリファクタリング

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -3,7 +3,6 @@ class AdminsController < ApplicationController
   def show
     @admin = Admin.find(params[:id])
     @preschools = @admin.preschools
-    # @preschool = Preschool.find(params[:id])
   end
 
   def index
